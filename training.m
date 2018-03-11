9ea66872855eb6ea2b148cc161ceca3e605745b6
xinkeyu <xinkeyu1997@gmail.com>
2018-03-10 21:15:02 -0800

    fix some typos

--- training.m
+++ training.m
@@ -1,4 +1,4 @@
-function [] = training (numOfLayers.neuronsPerLayer)
+function [weights,outs] = training (numOfLayers,neuronsPerLayer)
 %INPUT:
 %numOfLayers = number of layers (including input and output layers)
 %neuronsPerLayer = vector specifies the number of neurons in each layer
@@ -8,7 +8,7 @@ function [] = training (numOfLayers.neuronsPerLayer)
 
 %Preparation before training
 %weights is a collection of weight matrices 
-weights = cell(numOfLayers-1);
+weights = cell(numOfLayers-1,1);
 
 %TODO IN PART 5: assign a random number to each weight 
 for i = 1:numOfLayers-1
@@ -18,11 +18,11 @@ end
 
 %the target matrix is the identity matrix
 %for digit i-1, target(i,i) = 1 while the other columns in row i are 0
-target = eye(I);
+target = eye(10);
 
 %outs contains the OUT values for all layers except the input layer
-outs = cell(numOfLayers-1);
-for i = 1: numOfLayers %intialize to 0
+outs = cell(numOfLayers-1,1);
+for i = 1: numOfLayers-1 %intialize to 0
     outs{i} = zeros(1,neuronsPerLayer(i+1)); %input layer does not have OUT's
 end
 
